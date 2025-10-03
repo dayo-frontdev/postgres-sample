db9b769cec2a4f480d22bd21ad7fbd5827faaa34
dayo-frontdev <dayofrontdev@gmail.com>
2025-10-03 09:29:54 +0100

    postgres-server sample w dummy fe

---
+++ postgres-server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+const init = async () => {
+  app.get("/getsql", async (req, res) => {
+    const client = await pool.connect();
+    const [commentRes, boardRes] = await Promise.all([
+      client.query(
+        `
+                SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = $1
+                `,
+        [res.query.search]
+      ),
+      client.query(
+        `
+                SELECT * FROM boards WHERE board_id = 1$
+                `,
+        [res.query.search]
+      ),
+    ]);
+    res.json({
+      status: success,
+      board: boardRes.rows[0] || {},
+      posts: commentRes.rows,
+    });
+    await client.end();
+  });
+  app.use(express.static("./static"));
+
+  app.listen(5000);
+
+  console.log(" server listening on post 5000");
+};
+
+init();
