b293d81cd6284d3db58f9d30bd8aec579fe4143d
Olawale Dayo Ibrahim <dayofrontdev@gmail.com>
2025-10-03 15:11:40 +0100

    Add dotenv configuration for environment variables

--- postgres-server.js
+++ postgres-server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const { Pool } = require("pg");
-
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(express.json());
 
  app.use((req, res, next) => {
